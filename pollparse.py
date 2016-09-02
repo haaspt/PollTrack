1b4f426a53e03abb5cf7132d278dd46b1398495d
haaspt <patrick.tyler.haas@gmail.com>
2016-09-01 20:08:51 -0700

    Initial Commit: pollparse boilerplate

---
+++ pollparse.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+import pandas as pd
+import logging
+
+class PollParse(object):
+
+    def combine_polls(self, dataframe1, dataframe2):
+        """Combines two dataframes of polling data
+
+        Parameters
+        ----------
+        dataframe1, dataframe2: pandas dataframes
+            
+        Returns
+        -------
+        pandas dataframe of the combined data
+        """
+
+        #Actual code should go here
+        return combined_df
+
+    def rolling_average(self, dataframe):
+
+        #Code
+        
