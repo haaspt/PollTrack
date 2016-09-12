81839e98db4f6996f45c6257b4e29b9dc1b9b07e
haaspt <patrick.tyler.haas@gmail.com>
2016-09-12 14:04:24 -0700

    Tweak: removes an uncessecary import and adds a shebang on main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python
 from __future__ import print_function
 from pollio import PollIO
-from polltweet import PollTweet, Tweet
+from polltweet import PollTweet
 from pollparse import PollParse
 import os.path
 import json
