35a6f5e6924d9bb2cf63dd2adf86f463d0af88d0
Patrick Tyler Haas <patrick.tyler.haas@gmail.com>
2016-09-13 15:10:28 -0700

    Fix: variable scope problem with polltweet and other function

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ import logging
 class ConfigFileError(Exception):
     pass
 
-def get_and_tweet_new_polls(url):
+def get_and_tweet_new_polls(url, polltweet_instance):
 
     pollio = PollIO(url, "./data/", "data.csv")
     if pollio.new_poll_data is not None:
-        tweet_list = polltweet.pandas_to_tweet(pollio.new_poll_data)
-        polltweet.tweet_polls(tweet_list)
+        tweet_list = polltweet_instance.pandas_to_tweet(pollio.new_poll_data)
+        polltweet_instance.tweet_polls(tweet_list)
 
 def main():
 
@@ -27,15 +27,15 @@ def main():
             twitter_credentials = credentials['twitter_credentials']
             file.close()
 
-    global polltweet = PollTweet(twitter_credentials['consumer_key'],
-                                 twitter_credentials['consumer_secret'],
-                                 twitter_credentials['access_token_key'],
-                                 twitter_credentials['access_token_secret'])
+    polltweet = PollTweet(twitter_credentials['consumer_key'],
+                          twitter_credentials['consumer_secret'],
+                          twitter_credentials['access_token_key'],
+                          twitter_credentials['access_token_secret'])
 
     poll_url = "http://elections.huffingtonpost.com/pollster/2016-general-election-trump-vs-clinton.csv"
 
     while True:
-        get_and_tweet_new_polls(poll_url)
+        get_and_tweet_new_polls(poll_url, polltweet)
 
 if __name__ == "__main__":
     main()
