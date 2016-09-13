d9fda818165d549c58200855fcade6ba6fdef876
Patrick Tyler Haas <patrick.tyler.haas@gmail.com>
2016-09-13 16:20:24 -0700

    Adds: a whole bunch of logging messages

--- main.py
+++ main.py
@@ -23,20 +23,25 @@ def get_and_tweet_new_polls(url, polltweet_instance):
     polltweet_instance: PollTweet (object)
         Instance of the PollTweet object
     """
+    logger.debug("Attempting to get new polls")
     pollio = PollIO(url, "./data/", "data.csv")
     if pollio.new_poll_data is not None:
+        logger.debug("Attempting to tweet new polls")
         tweet_list = polltweet_instance.pandas_to_tweet(pollio.new_poll_data)
         polltweet_instance.tweet_polls(tweet_list)
         
 def main():
 
+    logger.info("PollTrack starting. Loading credentials")
     if not os.path.isfile('credentials.config'):
+        logger.error("Credentials file not found")
         raise CongigFileError("Credentials file not found, please see documentation for created a credential file")
     else:
         with open('credentials.config') as file:
             credentials = json.load(file)
             twitter_credentials = credentials['twitter_credentials']
             file.close()
+            logger.debug("Credentials loaded")
 
     polltweet = PollTweet(twitter_credentials['consumer_key'],
                           twitter_credentials['consumer_secret'],
@@ -45,6 +50,7 @@ def main():
 
     poll_url = "http://elections.huffingtonpost.com/pollster/2016-general-election-trump-vs-clinton.csv"
 
+    logger.info("Entering main loop")
     while True:
         get_and_tweet_new_polls(poll_url, polltweet)
 
